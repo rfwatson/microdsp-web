67d7f5d94ed4b488d6cb15a42747221677c25edd
Rob Watson <rob@netflux.io>
2025-09-09 06:50:24 +0200

    feat: bootstrap WASM module

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,27 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub use microdsp::mpm::MpmPitchDetector;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::js_sys;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+pub struct WasmPitchDetector(MpmPitchDetector);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[wasm_bindgen]
+impl WasmPitchDetector {
+    #[wasm_bindgen(constructor)]
+    pub fn new(sample_rate: f32, window_size: usize, hop_size: usize) -> Self {
+        Self(MpmPitchDetector::new(sample_rate, window_size, hop_size))
+    }
+
+    #[wasm_bindgen]
+    pub fn process(&mut self, buffer: &[f32], callback: &js_sys::Function) {
+        self.0.process(buffer, |res| {
+            callback
+                .call2(
+                    &JsValue::NULL,
+                    &JsValue::from_f64(res.frequency as f64),
+                    &JsValue::from_f64(res.clarity as f64),
+                )
+                .unwrap();
+        });
     }
 }
